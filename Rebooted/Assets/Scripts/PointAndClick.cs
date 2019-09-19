4b2ece656c9d10ac3184b87cf7689944e2d4c817
Sebastiaan <bobsebas@hotmail.nl>
2019-09-19 10:44:29 +0200

    player movement

--- Rebooted/Assets/Scripts/PointAndClick.cs
+++ Rebooted/Assets/Scripts/PointAndClick.cs
@@ -5,14 +5,52 @@ using UnityEngine;
 public class PointAndClick : MonoBehaviour
 {
     // Start is called before the first frame update
+    
+    private bool moving;
+    private bool gotHead;
     void Start()
     {
-        
+        moving = false;
+        gotHead = false;
     }
 
     // Update is called once per frame
+
+    public float speed;
+    private Ray ray;
+    private Vector3 target;
+    private RaycastHit hit;
+
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100)){
+                target = new Vector3(hit.point.x , hit.point.y + transform.position.y, hit.point.z);
+                transform.position = Vector3.MoveTowards(transform.position,target,speed);
+                moving = true;
+            }
+        }
+        if(moving){
+            transform.position = Vector3.MoveTowards(transform.position,target,speed);
+            if(Vector3.Distance(transform.position,target)< 0.3f){
+                moving = false;
+            }
+        }
     }
+    void OnTriggerEnter(Collider other)
+        {
+
+            if(other.gameObject.CompareTag("robotHead"))
+            {
+                other.gameObject.SetActive(false);
+                gotHead = true;
+            }
+            
+        }
+
+        bool getGotHead(){
+            return gotHead;
+        }
 }
