7c651815c65aeffe1964e37ac5696ac09ee590a6
KRademacher <Kevin_Rademacher@hotmail.com>
2019-10-03 11:39:40 +0200

    simple door mechanism

---
+++ Catacombes/Assets/Scripts/DoorMechanism.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DoorMechanism : MonoBehaviour
+{
+    public GameObject door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Block")
+        {
+            door.SetActive(false);
+        }
+    }
+}
