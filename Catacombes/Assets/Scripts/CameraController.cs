e687469cb77ee38d3edbfabc96ba50f7e676d8d9
KRademacher <Kevin_Rademacher@hotmail.com>
2019-10-03 11:01:53 +0200

    made player controller

---
+++ Catacombes/Assets/Scripts/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+
+    //Sensitivity controls
+    public float sensitivityX = 15f;
+    public float sensitivityY = 15f;
+
+    //Margin controls
+    public float minimumX = -360f;
+    public float maximumX = 360f;
+    public float minimumY = -60f;
+    public float maximumY = 60f;
+
+    //Y offset
+    public float rotationY = 0f;
+    
+    void Update()
+    {
+        switch (axes)
+        {
+            case RotationAxes.MouseXAndY: //Omni-directional controls
+
+                float rotationX = transform.localEulerAngles.y + (Input.GetAxis("Mouse X") * sensitivityX);
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+                transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+                break;
+
+            case RotationAxes.MouseX: //Horizontal only controls
+
+                transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+                break;
+
+            case RotationAxes.MouseY: //Vertical only controls
+
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+                transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+                break;
+        }
+    }
+}
---
+++ Catacombes/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private CharacterController controller;
+    private Vector3 direction = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            direction = Camera.main.transform.TransformDirection(direction);
+        }
+
+        direction.y += Physics.gravity.y * Time.deltaTime;
+        controller.Move(direction * moveSpeed * Time.deltaTime);
+    }
+}
